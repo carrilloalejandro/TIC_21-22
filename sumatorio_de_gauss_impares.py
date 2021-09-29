d0e389344e52234de2fd40da596c542722cc2e68
carrilloalejandro <90762565+carrilloalejandro@users.noreply.github.com>
2021-09-29 11:09:59 +0200

    Add files via upload

---
+++ sumatorio_de_gauss_impares.py
@@ -0,0 +1,10 @@
+def sumatorio_de_gauss_impares():
+    x=input("Hasta que numero de cosas: ")
+    acum=0
+    resto=0
+    for cont in range (0,x+1):
+        resto = cont %2
+        if(resto==1):
+            acum=acum+cont
+        print(str(cont) +"acum =" +str(acum) )
+sumatorio_de_gauss_impares()
