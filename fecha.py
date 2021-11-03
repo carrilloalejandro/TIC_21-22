d414d051fdc21d130ecd208c03c5ff262164a7b2
carrilloalejandro <90762565+carrilloalejandro@users.noreply.github.com>
2021-11-03 11:36:54 +0100

    Add files via upload

---
+++ fecha.py
@@ -0,0 +1,45 @@
+def fecha():
+    x=raw_input("INTRODUCE UNA FECHA dd/mm/aa: ")
+    y = x[3:5]
+    if(y=="01"):
+        print("Enhorabuena, feliz Enero")
+    else:
+        if(y=="02"):
+            print("Enhorabuena, feliz Febrero")
+        else:
+            if(y=="03"):
+                print("Enhorabuena, feliz Marzo")
+            else:
+                if(y=="04"):
+                    print("Enhorabuena, feliz Abril")
+                else:
+                    if(y=="05"):
+                        print("Enhorabuena, feliz Mayo")
+                    else:
+                        if(y=="06"):
+                            print("Enhorabuena, feliz Junio")
+                        else:
+                            if(y=="07"):
+                                print("Enhorabuena, feliz Julio")
+                            else:
+                                if(y=="08"):
+                                    print("Enhorabuena, feliz Agosto")
+                                else:
+                                    if(y=="09"):
+                                        print("Enhorabuena, feliz Septiempre")
+                                    else:
+                                        if(y=="10"):
+                                            print("Enhorabuena, feliz Octubre")
+                                        else:
+                                            if(y=="11"):
+                                                print("Enhorabuena, feliz Noviembre")
+                                            else:
+                                                if(y=="12"):
+
+
+                                                    print("Enhorabuena, feliz Diciembre")
+                                                else:
+                                                    print("NO FUNCIONO")
+                                           
+       
+fecha()
