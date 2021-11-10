fd2f26a303270faeeaf29479554a12e141f6dbc1
carrilloalejandro <90762565+carrilloalejandro@users.noreply.github.com>
2021-11-10 11:55:55 +0100

    Add files via upload

---
+++ menu_2.py
@@ -0,0 +1,62 @@
+def pinta_menu():
+    print("****************************")
+    print("*           MENU           *")
+    print("****************************")
+    print("1. SUMAR")
+    print("2. RESTAR")
+    print("3. MULTIPLICACION")
+    print("4. DIVISION")
+def manda_error():
+    print("ELECCION ERRONEA. NO PUEDO DIVIVIR POR 0")
+def suma(num1,num2):
+    respuesta=num1+num2
+    print(respuesta)
+def resta(num1,num2):
+    respuesta=num1-num2
+    print(respuesta)
+def multiplicacion(num1,num2):
+    respuesta=num1*num2
+    print(respuesta)
+def division(num1,num2):
+    respuesta=(float(num1)/num2)
+    print(respuesta)
+def menu_2():
+    #Pedimos dos numeros
+    error=1
+    while(error==1):
+        num1=input("Introduce un numero: ")
+        num2=input("introduce otro numero: ")
+        pinta_menu()
+        opcion=0
+        while(opcion<=0 or opcion>4):
+            opcion=input(" ELIGE: ")
+            if(num2==0 and opcion==4):
+                error=1
+                manda_error()
+            else:
+                error=0
+    print("Has elegido la opcion "+str(opcion))
+
+
+    
+    #OPCION SUMA
+    if(opcion==1):
+        print(suma(num1,num2))
+        #OPCION RESTA
+    else:
+        if(opcion==2):
+            print(resta(num1,num2))
+            #OPCION PRODUCTO
+        else:
+            if(opcion==3):
+               print(multiplicacion(num1,num2))
+                #OPCION COCIENTE   
+            else:
+                print(division(num1,num2))
+                
+    
+    
+    
+
+
+menu_2()
