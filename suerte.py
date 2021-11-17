d3ee77c3b818ed99c6f4b58b8bed02bae8e1830f
carrilloalejandro <90762565+carrilloalejandro@users.noreply.github.com>
2021-11-17 12:03:22 +0100

    Add files via upload

---
+++ suerte.py
@@ -0,0 +1,7 @@
+import random
+def suerte():
+    for cont in range (1,11):
+        num=random.randint(1,10)
+        print(num)
+suerte()
+# un programa de dados. 3 tiradas. Gana el que sume el numero mayor.  2 jugadores
